0b94e33da87bc9eecd3c2e05ecd279f21bc27545
Circuito Sapiens <111272833+NoobHackPro23@users.noreply.github.com>
2024-03-12 11:37:55 -0600

    Add files via upload

--- js/jav.js
+++ js/jav.js
@@ -1,33 +1,20 @@
-// shopping-cart.js
 
-// Crear una clase para el carrito de compras
-class ShoppingCart {
-    constructor() {
-        this.cartItems = []; // Array para almacenar los productos agregados
-    }
-
-    // Método para agregar un producto al carrito
-    addToCart(product) {
-        this.cartItems.push(product);
-        console.log(`Producto "${product.product-title}" agregado al carrito.`);
-    }
-
-    // Método para mostrar los productos en el carrito
-    displayCart() {
-        console.log("Productos en el carrito:");
-        this.cartItems.forEach((product, index) => {
-            console.log(`${index + 1}. ${product.name} - Precio: $${product.price}`);
-        });
-    }
+function opnA() {
+    ventanaA.style.display = "block";
+    ventanaP.style.display = "none";
 }
 
-// Ejemplo de uso
-const myCart = new ShoppingCart();
+function opnB() {
+    ventanaB.style.display = "block";
+    ventanaP.style.display = "none";
+}
 
-// Agregar productos al carrito
-myCart.addToCart({ name: "Camiseta", price: 25 });
-myCart.addToCart({ name: "Zapatos", price: 50 });
-myCart.addToCart({ name: "Pantalones", price: 40 });
+function opnC() {
+    ventanaC.style.display = "block";
+    ventanaP.style.display = "none";
+}
 
-// Mostrar los productos en el carrito
-myCart.displayCart();
+function opnA() {
+    ventanaA.style.display = "block";
+    ventanaP.style.display = "none";
+}
