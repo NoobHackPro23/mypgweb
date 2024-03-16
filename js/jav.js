9cb8a7441cd2a6e16ef3bfb33520b8365242e285
Circuito Sapiens <111272833+NoobHackPro23@users.noreply.github.com>
2024-03-16 08:06:05 -0600

    Update jav.js

--- js/jav.js
+++ js/jav.js
@@ -14,7 +14,19 @@ function opnC() {
     ventanaP.style.display = "none";
 }
 
-function opnA() {
-    ventanaA.style.display = "block";
-    ventanaP.style.display = "none";
+function blackMode() {
+    var body = document.body;
+    var links = document.getElementsByTagName('a');
+    var enca = document.querySelector('.encabezado');
+    var fondo = document.querySelector('.fondo');
+    var bor = document.querySelectorAll('.der a');
+
+    for(var i = 0; i < links.length; i++) {
+        links[i].classList.toggle("darkmode");
+    }
+
+    body.classList.toggle("darkmode");
+    enca.classList.toggle("darkmode");
+    fondo.classList.toggle("darkmode");
+    bor.classList.toggle("darkmode");
 }
